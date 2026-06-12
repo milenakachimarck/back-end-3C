f6b277733ab4e9bca96dc82ba129fb54e1699cf6
milenak <milena.kachimarck@escola.pr.gov.br>
2026-06-12 11:02:42 -0300

    código

--- primitivi/index.js
+++ primitivi/index.js
@@ -1,2 +1,28 @@
-let nome = "Milena"
-console.log(nome)
+//Tipo Number
+let idade = 30;
+console.log("idade:", idade)
+console.log(typeof idade)
+//Tipo String
+let nome ="Pedro";
+console.log ("nome:",nome);
+console.log(typeof nome);
+//tipo boolean
+let maiorDeIdade= true;
+console.log("maior de idade:", maiorDeIdade);
+console.log(typeof maiorDeIdade)
+//tipo undefined
+let endereço;
+console.log("endereço", endereço);
+console.logo(typeof endereço);
+//tipo null;
+let salario=null;
+console.log ("salário:",salario);
+console.log(typeof salario);
+//tipo symbol
+let simbolo=symbol("indentificador único");
+console.log("símbolo:", símbolo);
+console.log(typeof simbolo);
+//tipo blgInt
+let numberoGrande=BigInt(9007199254740991);
+console.log("número grande:", numberoGrande);
+console.log(typeof númeroGrande);
